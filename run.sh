504045fac37f04913b3d7166df00de37781a2012
Kartik Sreenivasan <ksreenivasa2@wisc.edu>
2021-04-04 14:55:35 +0000

    code cleanup and minor fixes
    also added some stuff to gitignore to make life easier

--- run.sh
+++ run.sh
@@ -17,17 +17,19 @@
 
 
 # # CIFAR-10, test (HC)
-# python main.py --config configs/hypercube/conv4/conv4_kn_hypercube.yml \
-#                --multigpu 1,2 \
-#                --name conv4-kn-unsigned-hypercube-adam-0.01--scale-fan_False \
-#                --prune-rate 0.0 \
+ python main.py --config configs/hypercube/conv4/conv4_kn_hypercube.yml \
+                --multigpu 1,2 \
+                --name conv4-kn-unsigned-hypercube-adam-0.01--scale-fan_False \
+                --prune-rate 0.0 \
 #                --pretrained 'models/pretrained/conv4-kn-unsigned-hypercube-adam-0.01--scale-fan_False.pth' \
 #                --evaluate 1
+#                --bias \
 
 
 ## CIFAR-10, train (EP)
-python main.py --config configs/hypercube/conv4/conv4_kn_unsigned.yml \
-              --multigpu 1,2 \
-              --name conv4-kn-unsigned \
-              --prune-rate 0.5 \
-              --bias\
+## Note: EP seems to do worse in the beginning with bias
+#python main.py --config configs/hypercube/conv4/conv4_kn_unsigned.yml \
+#              --multigpu 1,2 \
+#              --name conv4-kn-unsigned \
+#              --prune-rate 0.5 \
+##              --bias \
