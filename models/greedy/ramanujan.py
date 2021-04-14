d612d5bcefc49fe4b0205c2b913d397ad507358c
acnagle <acnagle@wisc.edu>
2021-04-14 16:10:09 -0500

    add greedy conv4 network

--- models/greedy/ramanujan.py
+++ models/greedy/ramanujan.py
@@ -41,10 +41,10 @@ class Net(nn.Module):
 class Conv4(nn.Module):
     def __init__(self, input_size, num_classes, args):
         super(Conv4, self).__init__()
-        self.conv1 = MaskConv(3, 64, kernel_size=(3, 3), stride=1, bias=args.bias)
-        self.conv2 = MaskConv(64, 64, kernel_size=(3, 3), stride=1, bias=args.bias)
-        self.conv3 = MaskConv(64, 128, kernel_size=(3, 3), stride=1, bias=args.bias)
-        self.conv4 = MaskConv(128, 128, kernel_size=(3, 3), stride=1, bias=args.bias)
+        self.conv1 = MaskConv(3, 64, kernel_size=(3, 3), stride=1, padding=1, bias=args.bias)
+        self.conv2 = MaskConv(64, 64, kernel_size=(3, 3), stride=1, padding=1, bias=args.bias)
+        self.conv3 = MaskConv(64, 128, kernel_size=(3, 3), stride=1, padding=1, bias=args.bias)
+        self.conv4 = MaskConv(128, 128, kernel_size=(3, 3), stride=1, padding=1, bias=args.bias)
         self.fc1 = MaskLinear(8192, 256, bias=args.bias)
         self.fc2 = MaskLinear(256, 256, bias=args.bias)
         self.fc3 = MaskLinear(256, num_classes, bias=args.bias)
@@ -72,7 +72,6 @@ class Conv4(nn.Module):
         x = self.fc2(x)
         x = F.relu(x)
         
-        x = self.fc3(x)
-        out = self.linear(out)
+        out = self.fc3(x)
         return out.squeeze()
 
