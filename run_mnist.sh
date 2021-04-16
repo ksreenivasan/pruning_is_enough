8a360409e6bc80e0600bc1a201d35bc883f80d7c
Kartik Sreenivasan <ksreenivasa2@wisc.edu>
2021-04-16 23:10:36 +0000

    adding mnist exps

--- run_mnist.sh
+++ run_mnist.sh
@@ -1,7 +1,19 @@
+:<<BLOCK
 python mnist_pruning_exps.py --mode pruning \
 --algo hc \
+--lmbda 0.00000 \
 --regularization \
---lmbda 0.00001
 --lr 0.01 \
 --optimizer adam \
+--wd 0 \
+--epochs 50
+BLOCK
+
+python mnist_pruning_exps.py --mode pruning \
+--algo hc \
+--lmbda 0.000001 \
+--regularization \
+--lr 0.01 \
+--optimizer adam \
+--wd 0 \
 --epochs 50
