59414188ba754c079ae08969e05bbaf998d971de
kamikazekartik <sreenivasan.kartik@gmail.com>
2021-04-21 21:59:24 -0500

    trying to automate plots and results

--- run_mnist.sh
+++ run_mnist.sh
@@ -4,10 +4,11 @@ python mnist_pruning_exps.py --mode pruning \
 --lr 0.01 \
 --optimizer adam \
 --wd 0 \
---epochs 50
+--epochs 50 \
+--results-filename resuls_acc_mnist_hc_noreg_adam.csv
 #BLOCK
 
-:<<BLOCK
+<<BLOCK
 python mnist_pruning_exps.py --mode pruning \
 --algo hc \
 --lmbda 0.000001 \
@@ -16,4 +17,14 @@ python mnist_pruning_exps.py --mode pruning \
 --optimizer adam \
 --wd 0 \
 --epochs 50
+--results-filename resuls_acc_mnist_hc_reg_adam.csv
+BLOCK
+
+
+<<BLOCK
+python mnist_pruning_exps.py --mode training \
+--lr 0.001 \
+--wd 0.0001 \
+--optimizer adam
+--results-filename resuls_acc_mnist_weight_train_adam.csv
 BLOCK
