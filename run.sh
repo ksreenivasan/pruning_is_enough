de367ab5c7c6e636bd455ad00f0456dce50adf4f
root <jysohn1108@gmail.com>
2021-04-23 16:01:44 +0000

    simplified with nohup

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#python main.py --config configs/hypercube/conv4/conv4_sc_hypercube.yml 
-#python main.py --config configs/hypercube/conv4/conv4_sc_hypercube_reg.yml 
-python main.py --config configs/hypercube/conv4/conv4_sc_hypercube_prob.yml 
-#python main.py --config configs/ep/conv4/conv4_sc_ep.yml
+#python main.py --config configs/hypercube/conv4/conv4_sc_hypercube_prob.yml 
+#python main.py --config configs/ep/conv4/conv4_sc_ep.yml > log_ep 2>&1
+#python main.py --config configs/hypercube/conv4/conv4_sc_hypercube.yml > log_hc_noreg_naive 2>&1 
+python main.py --config configs/hypercube/conv4/conv4_sc_hypercube_reg.yml #> log_hc_reg_naive 2>&1
 
