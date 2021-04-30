3fc7acfca65436bd1435f4132e190c3bd5c6f53b
acnagle <acnagle@wisc.edu>
2021-04-30 14:41:39 -0500

    small nomenclature update

--- args.py
+++ args.py
@@ -459,7 +459,7 @@ def parse_arguments():
         '--how-to-connect',
         type=str,
         default='prob',
-        help="procedure for interpolating the mask. Can be \"random\", which chooses a binary value for a mask coordinate with bern(alpha), \"soft\" which uses the [0, 1] score for each coordinate for interpolation (the mask consists of continuous values rather than binary in this case), and \"round\" which is uses the mask obtained from the \"soft\" option and applies naive rounding."
+        help="procedure for interpolating the mask. Can be \"random\", which chooses a binary value for a mask coordinate with bern(alpha), \"score\" which uses the [0, 1] score for each coordinate for interpolation (the mask consists of continuous values rather than binary in this case), and \"round\" which is uses the mask obtained from the \"score\" option and applies naive rounding."
     )
     parser.add_argument(
         "--no-cuda",
