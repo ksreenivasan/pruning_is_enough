6688f6318cc158eb93859dd439cd5fc9ac3537a8
Kartik Sreenivasan <ksreenivasa2@wisc.edu>
2021-06-06 17:08:34 +0000

    replacing comments with BLOCK in run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 # Conventional (Weight training, EP)
-# python main.py --config configs/training/conv4/conv4_training.yml
+:<<BLOCK
+python main.py --config configs/training/conv4/conv4_training.yml
+BLOCK
 
 # run EP/HC over multiple overparameterization setup (w/ SGD)
 :<<BLOCK
@@ -52,19 +54,23 @@ BLOCK
 
 
 # Compare naive rounding and probabilistic rounding
-# python main.py --config configs/hypercube/conv4/conv4_sc_hypercube_reg_compare_naive_prob.yml
-
+:<<BLOCK
+python main.py --config configs/hypercube/conv4/conv4_sc_hypercube_reg_compare_naive_prob.yml
+BLOCK
 
 #####################################################
 # RESNET18 CIFAR10 EXPERIMENTS ######################
 #####################################################
 
-# python main.py --config configs/ep/resnet18/resnet18_sc_ep.yml > log_resnet18_ep 2>&1
-#python main.py --config configs/hypercube/resnet18/resnet18_sc_hypercube_noreg.yml #> log_resnet18_hc 2>&1
+:<<BLOCK
+python main.py --config configs/ep/resnet18/resnet18_sc_ep.yml > log_resnet18_ep 2>&1
+python main.py --config configs/hypercube/resnet18/resnet18_sc_hypercube_noreg.yml #> log_resnet18_hc 2>&1
+BLOCK
 
+:<<BLOCK
 lmbda=(0.001) #0.01 0.0001 0.000001)
 for lm in ${lmbda[@]}
 do
     python main.py --config configs/hypercube/resnet18/resnet18_sc_hypercube_reg.yml --lmbda $lm # > log_resnet18_hc_lmbda_$lm 2>&1
 done
-
+BLOCK
