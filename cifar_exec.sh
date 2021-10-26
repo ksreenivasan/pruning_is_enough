4d3d3b819aaaf6e9185e96f30749737bc056c1f6
root <jysohn1108@gmail.com>
2021-10-26 20:24:12 +0000

    cifar 600 epochs

--- cifar_exec.sh
+++ cifar_exec.sh
@@ -2,12 +2,12 @@
 
 #### ResNet-18
 #python main.py --config configs/hypercube/resnet18/resnet18_sc_hypercube_reg.yml # 93.17% at 150 epoch
-#python main.py --config configs/hypercube/resnet18/resnet18_sc_hypercube_iter_reg.yml 
+python main.py --config configs/hypercube/resnet18/resnet18_sc_hypercube_iter_reg.yml 
 #python main.py --config configs/hypercube/resnet18/resnet18_sc_hypercube_iter_reg_v2.yml 
 #python main.py --config configs/hypercube/resnet18/resnet18_sc_hypercube_noreg.yml 
 #python main.py --config configs/ep/resnet18/resnet18_sc_ep.yml
 
-python main.py --config configs/hypercube/resnet18/resnet18_sc_hypercube_iter_reg_evaluate.yml 
+#python main.py --config configs/hypercube/resnet18/resnet18_sc_hypercube_iter_reg_evaluate.yml 
 
 
 
