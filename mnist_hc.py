86d6db884e00031b64a8f0ccf545dcdaab47579e
J.Sohn <jysohn1108@gmail.com>
2021-10-26 15:21:55 -0500

    fix the bug for sanity check on MNIST

--- mnist_hc.py
+++ mnist_hc.py
@@ -683,7 +683,8 @@ def switch_to_wt(model, device):
 
 
 def redraw(model, shuffle=False, mask=False):
-    for layer in [model.conv1, model.conv2, model.fc1, model.fc2]:
+    cp_model = copy.deepcopy(model)
+    for layer in [cp_model.conv1, cp_model.conv2, cp_model.fc1, cp_model.fc2]:
         #print(layer)
         #print(layer.weight)
         if shuffle:
@@ -696,7 +697,7 @@ def redraw(model, shuffle=False, mask=False):
         else:
             nn.init.kaiming_normal_(layer.weight, mode="fan_in", nonlinearity="relu")
         #print(layer.weight)
-    return
+    return cp_model
 
 
 def main():
@@ -901,17 +902,17 @@ def main():
         print("Sparsity: {:.2f}%\n".format(sparsity))
 
         # test shuffling weights 
-        redraw(model)#, device, criterion, test_loader)
+        model = redraw(model)#, device, criterion, test_loader)
         redraw_acc_list = test(model, device, criterion, test_loader)
         print("After redrawing weights")
         print("Test Acc: {:.2f}%\n".format(redraw_acc_list))
         
-        redraw(model, shuffle=True)#, device, criterion, test_loader)
+        model = redraw(model, shuffle=True)#, device, criterion, test_loader)
         shuff_acc_list = test(model, device, criterion, test_loader)
         print("After shuffling weights")
         print("Test Acc: {:.2f}%\n".format(shuff_acc_list))
 
-        redraw(model, shuffle=True, mask=True)#, device, criterion, test_loader)
+        model = redraw(model, shuffle=True, mask=True)#, device, criterion, test_loader)
         shuff_acc_list = test(model, device, criterion, test_loader)
         print("After shuffling masks")
         print("Test Acc: {:.2f}%\n".format(shuff_acc_list))
