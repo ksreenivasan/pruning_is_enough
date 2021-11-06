4748de9db5cdcaf2b32d6f356e9d6361e198f2c2
root <jysohn1108@gmail.com>
2021-11-06 02:01:01 +0000

    for running periodic rounding + bottom K

--- cifar_exec_GD.sh
+++ cifar_exec_GD.sh
@@ -6,9 +6,12 @@
      --config configs/hypercube/resnet20/resnet20_random_subnet.yaml > kartik_log 2>&1
 python main.py \
      --config configs/hypercube/resnet20/resnet20_quantized_hypercube_reg.yml #> cifar_run_log 2>&1
-BLOCK
 
 python main.py \
      --config configs/hypercube/resnet20/resnet20_quantized_hypercube_reg_GD.yml #> cifar_run_log 2>&1
+BLOCK
+
+python main.py \
+     --config configs/hypercube/resnet20/resnet20_sc_hypercube_reg_bottom_K_periodic_rounding.yml 
 
 
