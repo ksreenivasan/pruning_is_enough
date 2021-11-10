b5838420f8e85f812462915f2575000144f6e259
ksreenivasan <ksreenivasa2@wisc.edu>
2021-11-09 23:31:37 -0600

    deleting old sanity_check function
    it isn't used anywhere

--- main.py
+++ main.py
@@ -143,69 +143,6 @@ def finetune(model, parser_args, data, criterion, old_epoch_list, old_test_acc_b
     return model
 
 
-# TODO: This function is not used! Will either delete or repurpose 
-def sanity_check(model, parser_args, data, criterion):
-
-    # cp_model = redraw(model, shuffle=True)
-    cp_model = redraw(model, shuffle=True, mask=True)         # NOTE: uncomment to check the other case
-    # cp_model = copy.deepcopy(model)
-    cp_model = switch_to_wt(cp_model)
-    optimizer = get_optimizer(parser_args, cp_model)
-
-    run_base_dir, ckpt_base_dir, log_base_dir = get_directories(parser_args)
-    parser_args.ckpt_base_dir = ckpt_base_dir
-    writer = SummaryWriter(log_dir=log_base_dir)
-    epoch_time = AverageMeter("epoch_time", ":.4f", write_avg=False)
-    validation_time = AverageMeter("validation_time", ":.4f", write_avg=False)
-    train_time = AverageMeter("train_time", ":.4f", write_avg=False)
-    progress_overall = ProgressMeter(
-        1, [epoch_time, validation_time, train_time], prefix="Overall Timing"
-    )
-
-    end_epoch = time.time()
-    parser_args.start_epoch = parser_args.start_epoch or 1
-
-    results = {'epoch': [], 'test_acc': []}
-
-
-    cp_model.cuda(parser_args.gpu)
-    acc1, acc5, acc10 = validate(data.val_loader, cp_model, criterion, parser_args, writer=None, epoch=0)
-
-    results['epoch'].append(0)
-    results['test_acc'].append(acc1)
-
-    for epoch in range(parser_args.start_epoch, parser_args.epochs + 1):
-        if parser_args.multiprocessing_distributed:
-            data.train_loader.sampler.set_epoch(epoch)
-
-        lr_policy(epoch, iteration=None)
-        modifier(parser_args, epoch, cp_model)
-
-        cur_lr = get_lr(optimizer)
-        print('cur_lr: ', cur_lr)
-
-        # train for one epoch
-        start_train = time.time()
-        train_acc1, train_acc5, train_acc10, reg_loss = train(
-            data.train_loader, cp_model, criterion, optimizer, epoch, parser_args, writer=writer
-        )
-        train_time.update((time.time() - start_train) / 60)
-
-        # evaluate on validation set
-        start_validation = time.time()
-        acc1, acc5, acc10 = validate(data.val_loader, cp_model, criterion, parser_args, writer, epoch)
-        validation_time.update((time.time() - start_validation) / 60)
-
-        results['epoch'].append(epoch)
-        results['test_acc'].append(acc1)
-
-        results_df = pd.DataFrame.from_dict(results).set_index('epoch')
-        #results_df.to_csv('./results/shuffle_weights.csv')            # NOTE: be sure to update filename accordingly!
-        results_df.to_csv('./results/shuffle_mask.csv')
-        #results_df.to_csv('./results/orig_mask.csv')
-
-    sys.exit()
-
 def get_idty_str(parser_args):
     train_mode_str = 'weight_training' if parser_args.weight_training else 'pruning'
     dataset_str = parser_args.dataset
