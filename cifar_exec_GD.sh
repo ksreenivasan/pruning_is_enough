3bc425645e4ba016c1a353719a1fd0a79fd06d4d
root <jysohn1108@gmail.com>
2021-11-10 08:35:12 +0000

    minor change

--- cifar_exec_GD.sh
+++ cifar_exec_GD.sh
@@ -15,9 +15,11 @@
 #python main.py --config configs/hypercube/resnet20/resnet20_sc_hypercube_reg_GD.yml
 
 
-python main.py \
-    --config configs/hypercube/resnet20/resnet20_quantized_hypercube_reg.yml #> cifar_run_log 2>&1
+#python main.py \
+#    --config configs/hypercube/resnet20/resnet20_quantized_hypercube_reg.yml #> cifar_run_log 2>&1
 
+python main.py \
+    --config configs/hypercube/resnet20/resnet20_sc_hypercube_reg_bottom_K_periodic_rounding.yml
 
 
 #python main.py --config configs/hypercube/resnet20/resnet20_wt.yml
