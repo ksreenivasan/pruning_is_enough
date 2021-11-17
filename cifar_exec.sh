1816de8b32324ae7e772152787b9600d77c25385
Leiay <yllucy@yeah.net>
2021-11-17 04:12:39 +0000

    push to branch, and load the code locally

--- cifar_exec.sh
+++ cifar_exec.sh
@@ -13,13 +13,13 @@
 #    --config configs/hypercube/resnet20/resnet20_random_subnet.yaml > kartik_log 2>&1
 BLOCK
 
-:<<BLOCK
-# python main.py --config configs/hypercube/resnet20/resnet20_sc_hypercube_reg_GD.yml
-BLOCK
+#:<<BLOCK
+#python main.py --config configs/hypercube/resnet20/resnet20_sc_hypercube_reg_GD.yml
+#BLOCK
 
 # attempt at getting 1.4% sparsity with 80% acc
 python main.py \
-    --config configs/hypercube/resnet20/resnet20_quantized_hypercube_reg_bottom_K.yml > cifar_log 2>& 1
+    --config configs/hypercube/resnet20/resnet20_quantized_hypercube_reg_bottom_K.yml
 
 # python main.py --config configs/hypercube/resnet20/resnet20_wt.yml
 
