f3af74028b589693342c3f49ac0d9381172b21a2
root <jysohn1108@gmail.com>
2021-11-23 17:58:12 +0000

    non-iterative wild pruning

--- cifar_exec_GD.sh
+++ cifar_exec_GD.sh
@@ -14,14 +14,22 @@ init_list=(unif)
 #init_list=(skew unif)
 for init in ${init_list[@]}
 do
-#	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov20_v2.yml \
-#		--iter_period 25 --prune-rate 0.55 --score-init $init --gpu 1 --lmbda 0.0001
-#	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov20_v2.yml \
-#		--iter_period 25 --prune-rate 0.55 --score-init $init --gpu 2 --lmbda 0.0001 --fine-tune-lr 0.1
-#	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov20_v2.yml \
-#		--iter_period 25 --prune-rate 0.55 --score-init $init --gpu 2 --lmbda 0 --fine-tune-lr 0.1
-	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov22_3pm.yml \
-		--iter_period 8 --prune-rate 0.2 --score-init $init --gpu 3 --lmbda 0.01 --fine-tune-lr 0.1
+#	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov22_8pm_SGD.yml \
+#		--iter_period 8 --prune-rate 0.2 --score-init $init --gpu 3 --lmbda 0 --fine-tune-lr 0.1
+#	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov22_8pm_SGD.yml \
+#		--iter_period 8 --prune-rate 0.2 --score-init $init --gpu 3 --lmbda 0.000001 --fine-tune-lr 0.1
+#	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov22_10pm_SGD.yml \
+#		--iter_period 8 --prune-rate 0.2 --score-init $init --gpu 3 --lmbda 0.00005 --fine-tune-lr 0.01 \
+#		--regularization L1 --lr-policy constant_lr  
+#	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov22_10pm_SGD.yml \
+#		--iter_period 8 --prune-rate 0.2 --score-init $init --gpu 3 --lmbda 0.00005 --fine-tune-lr 0.01 \
+#		--regularization L1 --lr-policy multistep_lr 
+#	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov22_10pm_SGD.yml \
+#		--iter_period 8 --prune-rate 0.2 --score-init $init --gpu 3 --lmbda 0.00005 --fine-tune-lr 0.01 \
+#		--regularization L1 --lr-policy cosine_lr 
+	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov23_11am_SGD.yml \
+		--iter_period 49 --prune-rate 0.98 --score-init $init --gpu 3 --lmbda 0.00005 --fine-tune-lr 0.01 \
+		--regularization L1 --lr-policy cosine_lr
 done
 
 
