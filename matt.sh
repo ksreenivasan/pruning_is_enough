8e16b55c55182e1384824707819b8b0b0f2bfcc4
Matthew Grinde <mgrinde2@wisc.edu>
2021-12-05 19:53:15 +0000

    updates from running

--- matt.sh
+++ matt.sh
@@ -10,6 +10,6 @@ do
 #		--iter_period 25 --prune-rate 0.55 --score-init $init --gpu 2 --lmbda 0.0001 --fine-tune-lr 0.1
 #	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov20_v2.yml \
 #		--iter_period 25 --prune-rate 0.55 --score-init $init --gpu 2 --lmbda 0 --fine-tune-lr 0.1
-	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov22_3pm.yml \
-		--iter_period 5 --prune-rate 0.5 --score-init $init --gpu 2 --lmbda 0.00005 --fine-tune-lr 0.01
+	python main.py --config configs/hypercube/resnet20/resnet20_hypercube_bottom_K_Nov23_11am_SGD.yml \
+		--iter_period 6 --prune-rate 0.4 --score-init $init --gpu 2 --lmbda 0.00005 --fine-tune-lr 0.01
 done
