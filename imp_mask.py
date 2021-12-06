44aa129a8cf6f646d5f498f60e15ecb6384591ec
Leiay <yllucy@yeah.net>
2021-12-06 03:38:38 +0000

    remove the MIT license that was inherit from Frankle's repo. Correctly test the result where the log shows

--- imp_mask.py
+++ imp_mask.py
@@ -1,7 +1,6 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
+# this code is modified based on
+# https://github.com/facebookresearch/open_lth/blob/main/pruning/mask.py
 
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
 
 import numpy as np
 import torch
