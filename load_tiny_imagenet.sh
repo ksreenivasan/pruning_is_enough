e44340ed48fc71a7ccb3adfed5ad8b626d6d8138
root <jysohn1108@gmail.com>
2022-01-06 23:58:15 +0000

    change folder name

--- load_tiny_imagenet.sh
+++ load_tiny_imagenet.sh
@@ -1,3 +1,4 @@
+# from https://github.com/tjmoon0104/pytorch-tiny-imagenet/blob/master/run.sh
 echo "Loading TinyImageNet dataset..."
 wget -nc http://cs231n.stanford.edu/tiny-imagenet-200.zip
 unzip tiny-imagenet-200.zip
@@ -5,7 +6,7 @@ rm -r ./tiny-imagenet-200/test
 python3 val_format.py
 find . -name "*.txt" -delete
 mv tiny-imagenet-200 data/datasets/
-mv data/datasets/tiny-imagenet-200 data/datasets/tinyimagenet
+#mv data/datasets/tiny-imagenet-200 data/datasets/tinyimagenet
 
 
 
