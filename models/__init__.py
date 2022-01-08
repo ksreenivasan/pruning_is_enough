ccc49de6cb7a5b7eb5d6db31ebd239d45e79923a
J.Sohn <jysohn1108@gmail.com>
2022-01-07 21:54:56 -0600

    add resnet20Normal, resnet32Normal

--- models/__init__.py
+++ models/__init__.py
@@ -1,21 +1,22 @@
 from models.resnet import ResNet18, ResNet50, ResNet101, WideResNet50_2, WideResNet101_2
-from models.resnet_cifar import cResNet18, cResNet50, cResNet101
-from models.resnet_tiny import TinyResNet18
-from models.resnet_kaiming import resnet20
-from models.resnet_normal import TinyResNet18Normal
+from models.resnet_kaiming import resnet20, resnet32, resnet20Normal, resnet32Normal
 from models.frankle import FC, Conv2, Conv4, Conv4Normal, Conv6, Conv4Wide, Conv8, Conv6Wide
+
+#### TODO: delete below ones (merge with above code)
+from models.resnet_cifar import cResNet18, cResNet50 
+from models.resnet_tiny import TinyResNet18 
+from models.resnet_normal import TinyResNet18Normal
 
 __all__ = [
     "ResNet18",
     "ResNet50",
     "ResNet101",
-    "cResNet18",
-    "cResNet50",
-    "TinyResNet18",
-    "TinyResNet18Normal",
     "WideResNet50_2",
     "WideResNet101_2",
     "resnet20",
+    "resnet32",
+    "resnet20Normal",
+    "resnet32Normal",
     "FC",
     "Conv2",
     "Conv4",
@@ -24,4 +25,8 @@ __all__ = [
     "Conv4Wide",
     "Conv8",
     "Conv6Wide",
+    "cResNet18",
+    "cResNet50",
+    "TinyResNet18",
+    "TinyResNet18Normal",
 ]
