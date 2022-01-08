ee1d39515df6216b861670dee4ebca7f81dff949
J.Sohn <jysohn1108@gmail.com>
2022-01-08 00:43:56 -0600

    mobilenet

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 from models.resnet import ResNet18, ResNet50, ResNet101, WideResNet50_2, WideResNet101_2
 from models.resnet_kaiming import resnet20, resnet32, resnet20Normal, resnet32Normal
 from models.frankle import FC, Conv2, Conv4, Conv4Normal, Conv6, Conv4Wide, Conv8, Conv6Wide
+from models.mobilenet import MobileNetV2Normal
 
 #### TODO: delete below ones (merge with above code)
 from models.resnet_cifar import cResNet18, cResNet50 
