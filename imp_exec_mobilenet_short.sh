82eaa81e8233e8e1f098ebba7b793b5ea1303624
Leiay <yllucy@yeah.net>
2022-01-11 16:59:15 +0000

    comment on the sanity check part to save time

--- imp_exec_mobilenet_short.sh
+++ imp_exec_mobilenet_short.sh
@@ -5,12 +5,12 @@
 subfd="short_warm_imp_mobilenet"
 n_gpu=2
 
-python imp_main.py \
---config configs/imp/mobilenet.yml \
---iter-period 15 \
---epochs 300 \
---gpu $n_gpu \
---subfolder $subfd
+# python imp_main.py \
+# --config configs/imp/mobilenet.yml \
+# --iter-period 15 \
+# --epochs 300 \
+# --gpu $n_gpu \
+# --subfolder $subfd
 
 # for sanity check
 for i in 19 18 14 13 7 3 1 0 17 16 15 12 11 10 9 8 6 5 4 2
