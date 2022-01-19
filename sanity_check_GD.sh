7336a35be171c24b479de3279d3d5da5a790844e
root <jysohn1108@gmail.com>
2022-01-19 07:29:11 +0000

    merge

--- sanity_check_GD.sh
+++ sanity_check_GD.sh
@@ -2,8 +2,10 @@
 
 # Local
 #python main.py --config configs/hypercube/mobilenetV2/sparsity_20_3lam6_invert.yml > log_mobilev2_HC_sparsity_20_3lam6_invert 2>&1 
+python main.py --config configs/hypercube/tinyImageNet/resnet18/resnet18_sparsity_50_sgd_lam0_unflag_F_invert.yml > log_tiny_resnet18_sparsity_50_invert 2>&1
+
 
 # AWS1
 #python main.py --config configs/hypercube/mobilenetV2/sparsity_5_7lam6_unflag_True_invert.yml > log_mobilev2_HC_sparsity_5_7lam6_unflag_True_invert 2>&1 
 #python main.py --config configs/hypercube/mobilenetV2/sparsity_1_4_invert.yml > log_mobilev2_HC_sparsity_1_4_invert 2>&1
-python main.py --config configs/hypercube/mobilenetV2/sparsity_50_invert.yml > log_mobilev2_HC_sparsity_50_invert 2>&1
+#python main.py --config configs/hypercube/mobilenetV2/sparsity_50_invert.yml > log_mobilev2_HC_sparsity_50_invert 2>&1
