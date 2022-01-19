10c95cad0edd66bddbb32f96179fe9f247818e30
Ubuntu <ubuntu@ip-172-31-74-112.ec2.internal>
2022-01-19 07:25:14 +0000

    update from aws1

--- sanity_check_GD.sh
+++ sanity_check_GD.sh
@@ -1,3 +1,9 @@
 # MobileNetV2
 
-python main.py --config configs/hypercube/mobilenetV2/sparsity_20_3lam6_invert.yml > log_mobilev2_HC_sparsity_20_3lam6_invert 2>&1 
+# Local
+#python main.py --config configs/hypercube/mobilenetV2/sparsity_20_3lam6_invert.yml > log_mobilev2_HC_sparsity_20_3lam6_invert 2>&1 
+
+# AWS1
+#python main.py --config configs/hypercube/mobilenetV2/sparsity_5_7lam6_unflag_True_invert.yml > log_mobilev2_HC_sparsity_5_7lam6_unflag_True_invert 2>&1 
+#python main.py --config configs/hypercube/mobilenetV2/sparsity_1_4_invert.yml > log_mobilev2_HC_sparsity_1_4_invert 2>&1
+python main.py --config configs/hypercube/mobilenetV2/sparsity_50_invert.yml > log_mobilev2_HC_sparsity_50_invert 2>&1
