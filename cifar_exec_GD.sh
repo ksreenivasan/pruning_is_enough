5e0f07308eed6499f2cdfded5f1a493ee9ef1eda
root <jysohn1108@gmail.com>
2022-01-19 06:55:16 +0000

    run file

--- cifar_exec_GD.sh
+++ cifar_exec_GD.sh
@@ -7,14 +7,12 @@
 
 #python main.py --config configs/training/mobilenetV2/cifar10_mobileV2_training_check.yml #> mobilenet_cifar10_wt_check
 
-#:<<BLOCK
+:<<BLOCK
 #python main.py --config configs/ep/mobilenetV2/cifar10_mobileV2_ep_sparsity_50.yml > log_mobilev2_EP_sparsity_50 2>&1
 #python main.py --config configs/ep/mobilenetV2/cifar10_mobileV2_ep_sparsity_5.yml > log_mobilev2_EP_sparsity_5 2>&1
 #python main.py --config configs/ep/mobilenetV2/cifar10_mobileV2_ep_sparsity_20.yml > log_mobilev2_EP_sparsity_20 2>&1
 #python main.py --config configs/ep/mobilenetV2/cifar10_mobileV2_ep_sparsity_1_7.yml > log_mobilev2_EP_sparsity_1_7 2>&1
-
-
-#BLOCK
+BLOCK
 
 :<<BLOCK
 python main.py --config configs/hypercube/mobilenetV2/sparsity_50.yml > log_mobilev2_HC_sparsity_50 2>&1 
@@ -23,6 +21,7 @@ python main.py --config configs/hypercube/mobilenetV2/sparsity_20.yml > log_mobi
 BLOCK
 #python main.py --config configs/hypercube/mobilenetV2/sparsity_5_7lam6.yml > log_mobilev2_HC_sparsity_5_7lam6 2>&1 
 #python main.py --config configs/hypercube/mobilenetV2/sparsity_20_1lam6.yml > log_mobilev2_HC_sparsity_20_1lam6 2>&1 
+#python main.py --config configs/hypercube/mobilenetV2/sparsity_20_3lam6.yml > log_mobilev2_HC_sparsity_20_3lam6 2>&1 
 
 
 #### ResNet-18
@@ -39,7 +38,7 @@ BLOCK
 
 
 ### ResNet-20
-python main.py --config configs/training/resnet20/cifar10_resnet20_training.yml
+#python main.py --config configs/training/resnet20/cifar10_resnet20_training.yml
 #python main.py --config configs/hypercube/resnet20/error_bar/resnet20_sparsity_3_72_t1.yml #> log_hc_sparsity_3_72_t1 2>&1
 
 
