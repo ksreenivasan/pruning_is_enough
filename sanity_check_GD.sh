4a67155262d20ae7ad57650338ed6c7bdbc46a0e
Ubuntu <ubuntu@ip-172-31-74-112.ec2.internal>
2022-01-20 06:15:17 +0000

    invert sanity check

--- sanity_check_GD.sh
+++ sanity_check_GD.sh
@@ -8,7 +8,7 @@
 # AWS1
 #python main.py --config configs/hypercube/mobilenetV2/sparsity_5_7lam6_unflag_True_invert.yml > log_mobilev2_HC_sparsity_5_7lam6_unflag_True_invert 2>&1 
 #python main.py --config configs/hypercube/mobilenetV2/sparsity_1_4_invert.yml > log_mobilev2_HC_sparsity_1_4_invert 2>&1
-#python main.py --config configs/hypercube/mobilenetV2/sparsity_50_invert.yml > log_mobilev2_HC_sparsity_50_invert 2>&1
-python main.py --config configs/hypercube/tinyImageNet/resnet18/resnet18_sparsity_5_adam_8lam6_invert.yml > log_tiny_resnet18_sparsity_5_invert 2>&1
+python main.py --config configs/hypercube/mobilenetV2/sparsity_50_invert.yml > log_mobilev2_HC_sparsity_50_invert 2>&1
+#python main.py --config configs/hypercube/tinyImageNet/resnet18/resnet18_sparsity_5_adam_8lam6_invert.yml > log_tiny_resnet18_sparsity_5_invert 2>&1
 
 
