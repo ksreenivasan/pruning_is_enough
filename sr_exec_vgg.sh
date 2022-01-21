3855a71256e18fa7e101bf827b4663601093b372
Liu Yang <yllucy@yeah.net>
2022-01-21 00:53:09 -0600

    add for smart ratio vgg

--- sr_exec_vgg.sh
+++ sr_exec_vgg.sh
@@ -4,27 +4,25 @@ config_file=configs/sr/vgg_sr.yml
 python main.py \
     --config $config_file \
     --smart_ratio 0.95 \
-    --subfolder sc_vgg_95 \
+    --subfolder sc_vgg_95_correct \
     --gpu $n_gpu
 
 python main.py \
     --config $config_file \
-    --smart_ratio 0.995 \
-    --subfolder sc_vgg_995 \
+    --smart_ratio 0.983 \
+    --subfolder sc_vgg_983_correct \
     --gpu $n_gpu
 
-<<:BLOCK
 python main.py \
     --config $config_file \
     --smart_ratio 0.5 \
-    --subfolder sc_vgg_50 \
+    --subfolder sc_vgg_50_correct \
     --gpu $n_gpu
 
 
 python main.py \
     --config $config_file \
     --smart_ratio 0.9 \
-    --subfolder sc_vgg_90 \
+    --subfolder sc_vgg_90_correct \
     --gpu $n_gpu
 
-:BLOCK
