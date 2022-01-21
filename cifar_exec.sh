0492779c2a0a42ada56bde8aaea939d5b8535238
ksreenivasan <ksreenivasa2@wisc.edu>
2022-01-20 19:57:40 -0600

    updating parallel run command to fix typo

--- cifar_exec.sh
+++ cifar_exec.sh
@@ -63,14 +63,16 @@ BLOCK
 
 
 # Running trials in parallel
+# NOTE: make sure to delete/comment subfolder from the config file or else it may not work
 conf_file="configs/hypercube/wideresnet28/wideresnet28_weight_training.yml"
 log_root="wideresnet28_"
 log_end="_log"
-subfolder_root="wideresnet28_results_"
+subfolder_root="wideresnet28_results_trial_"
 
 for trial in 2 3
 do
     python main.py \
-    --config "$conf_file" > "$log_root$trial$log_end" \
-    --subfolder "$subfolder_root$trial" 2>&1 &
+    --config "$conf_file" \
+    --trial-num $trial \
+    --subfolder "$subfolder_root$trial" > "$log_root$trial$log_end" 2>&1 &
 done
