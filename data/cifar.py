b5b1a94625428e669ee94bf10d68975de6f62e8d
ksreenivasan <ksreenivasa2@wisc.edu>
2022-01-21 18:55:33 -0600

    modifying data loader to use ffcv

--- data/cifar.py
+++ data/cifar.py
@@ -24,87 +24,58 @@ class CIFAR10:
     def __init__(self, args):
         super(CIFAR10, self).__init__()
 
-        data_root = os.path.join(parser_args.data, "cifar10")
 
-        use_cuda = torch.cuda.is_available()
+    data_root = os.path.join(parser_args.data, "cifar10")
+    use_cuda = torch.cuda.is_available()
+    # Data loading code
+    kwargs = {"num_workers": parser_args.workers, "pin_memory": True} if use_cuda else {}
 
-        # Data loading code
-        kwargs = {"num_workers": parser_args.workers, "pin_memory": True} if use_cuda else {}
-
-        normalize = transforms.Normalize(
-            mean=[0.491, 0.482, 0.447], std=[0.247, 0.243, 0.262]
-        )
-
-        train_dataset = torchvision.datasets.CIFAR10(
-            root=data_root,
-            train=True,
-            download=True,
-            transform=transforms.Compose(
-                [
-                    transforms.RandomCrop(32, padding=4),
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor(),
-                    normalize,
-                ]
-            ),
-        )
-        self.train_loader = torch.utils.data.DataLoader(
-            train_dataset, batch_size=parser_args.batch_size, shuffle=True, **kwargs
-        )
-
-        test_dataset = torchvision.datasets.CIFAR10(
-            root=data_root,
-            train=False,
-            download=True,
-            transform=transforms.Compose([transforms.ToTensor(), normalize]),
-        )
-        self.val_loader = torch.utils.data.DataLoader(
-            test_dataset, batch_size=parser_args.batch_size, shuffle=False, **kwargs
-        )
-datasets = {
+    datasets = {
     'train': torchvision.datasets.CIFAR10('/tmp', train=True, download=True),
     'test': torchvision.datasets.CIFAR10('/tmp', train=False, download=True)
-}
+    }
 
-for (name, ds) in datasets.items():
-    writer = DatasetWriter(f'/tmp/cifar_{name}.beton', {
-        'image': RGBImageField(),
-        'label': IntField()
-    })
-    writer.from_indexed_dataset(ds)
+    for (name, ds) in datasets.items():
+        writer = DatasetWriter(f'/tmp/cifar_{name}.beton', {
+            'image': RGBImageField(),
+            'label': IntField()
+        })
+        writer.from_indexed_dataset(ds)
 
-# Note that statistics are wrt to uin8 range, [0,255].
-CIFAR_MEAN = [125.307, 122.961, 113.8575]
-CIFAR_STD = [51.5865, 50.847, 51.255]
+    # Note that statistics are wrt to uin8 range, [0,255].
+    CIFAR_MEAN = [125.307, 122.961, 113.8575]
+    CIFAR_STD = [51.5865, 50.847, 51.255]
 
-BATCH_SIZE = 512
+    BATCH_SIZE = 512
 
-loaders = {}
-for name in ['train', 'test']:
-    label_pipeline: List[Operation] = [IntDecoder(), ToTensor(), ToDevice('cuda:0'), Squeeze()]
-    image_pipeline: List[Operation] = [SimpleRGBImageDecoder()]
+    loaders = {}
+    for name in ['train', 'test']:
+        label_pipeline: List[Operation] = [IntDecoder(), ToTensor(), ToDevice('cuda:0'), Squeeze()]
+        image_pipeline: List[Operation] = [SimpleRGBImageDecoder()]
 
-    # Add image transforms and normalization
-    if name == 'train':
+        # Add image transforms and normalization
+        if name == 'train':
+            image_pipeline.extend([
+                RandomHorizontalFlip(),
+                RandomTranslate(padding=2),
+                Cutout(8, tuple(map(int, CIFAR_MEAN))), # Note Cutout is done before normalization.
+            ])
         image_pipeline.extend([
-            RandomHorizontalFlip(),
-            RandomTranslate(padding=2),
-            Cutout(8, tuple(map(int, CIFAR_MEAN))), # Note Cutout is done before normalization.
+            ToTensor(),
+            ToDevice('cuda:0', non_blocking=True),
+            ToTorchImage(),
+            Convert(ch.float16),
+            torchvision.transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
         ])
-    image_pipeline.extend([
-        ToTensor(),
-        ToDevice('cuda:0', non_blocking=True),
-        ToTorchImage(),
-        Convert(ch.float16),
-        torchvision.transforms.Normalize(CIFAR_MEAN, CIFAR_STD),
-    ])
 
-    # Create loaders
-    loaders[name] = Loader(f'/tmp/cifar_{name}.beton',
-                            batch_size=BATCH_SIZE,
-                            num_workers=8,
-                            order=OrderOption.RANDOM,
-                            drop_last=(name == 'train'),
-                            pipelines={'image': image_pipeline,
-                                       'label': label_pipeline})
+        # Create loaders
+        loaders[name] = Loader(f'/tmp/cifar_{name}.beton',
+                                batch_size=BATCH_SIZE,
+                                num_workers=8,
+                                order=OrderOption.RANDOM,
+                                drop_last=(name == 'train'),
+                                pipelines={'image': image_pipeline,
+                                           'label': label_pipeline})
 
+    self.train_loader = loaders['train']
+    self.val_loader = loaders['test']
