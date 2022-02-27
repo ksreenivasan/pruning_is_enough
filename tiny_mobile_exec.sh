4d88d5872f149f40999d48c630ef40acdd5a63f4
Leiay <yllucy@yeah.net>
2022-02-27 22:02:04 +0000

    add mobilenet tiny exec file

---
+++ tiny_mobile_exec.sh
@@ -0,0 +1,23 @@
+
+# target sparsity 0.5
+
+python main.py \
+--config configs/hypercube/tinyImageNet/mobilenetV2/sparsity_50.yml \
+--subfolder tiny_mobile_target_sparsity_50 \
+--gpu 0 &
+
+python main.py \
+--config configs/hypercube/tinyImageNet/mobilenetV2/sparsity_5.yml \
+--subfolder tiny_mobile_target_sparsity_5 \
+--gpu 1 &
+
+python main.py \
+--config configs/hypercube/tinyImageNet/mobilenetV2/sparsity_2_5.yml \
+--subfolder tiny_mobile_target_sparsity_2_5 \
+--gpu 3 &
+
+python main.py \
+--config configs/hypercube/tinyImageNet/mobilenetV2/sparsity_1_4.yml \
+--subfolder tiny_mobile_target_sparsity_1_4 \
+--gpu 3 &
+
