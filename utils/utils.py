edc87b8693438983c855f3e28047efa0d7d03401
ksreenivasan <ksreenivasa2@wisc.edu>
2022-04-07 13:49:35 -0500

    changing benchmark for faster imagenet run

--- utils/utils.py
+++ utils/utils.py
@@ -25,9 +25,9 @@ def set_seed(seed):
     np.random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
     # making sure GPU runs are deterministic even if they are slower
-    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.deterministic = False
     # this causes the code to vary across runs. I don't want that for now.
-    # torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = True
     print("Seeded everything: {}".format(seed))
 
 
