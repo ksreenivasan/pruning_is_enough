49aab991abdc0d2f92105ff27a467f8e4384cc1b
ksreenivasan <ksreenivasa2@wisc.edu>
2022-04-25 16:12:53 -0500

    trying ddp with cifar10

--- ddp_poc.py
+++ ddp_poc.py
@@ -22,11 +22,11 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 # For TcpStore, same way as on Linux.
 
 def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
     os.environ['MASTER_PORT'] = '12355'
 
     # initialize the process group
-    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
 
 def cleanup():
     dist.destroy_process_group()
@@ -43,6 +43,23 @@ class ToyModel(nn.Module):
         return self.net2(self.relu(self.net1(x)))
 
 
+ def evaluate(model, device, test_loader):
+    model.eval()
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    accuracy = correct / total
+
+    return accuracy
+
+
 def demo_basic(rank, world_size):
     print(f"Running basic DDP example on rank {rank}.")
     setup(rank, world_size)
@@ -51,14 +68,57 @@ def demo_basic(rank, world_size):
     model = ToyModel().to(rank)
     ddp_model = DDP(model, device_ids=[rank])
 
+    transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    # Data should be prefetched
+    # Download should be set to be False, because it is not multiprocess safe
+    train_set = torchvision.datasets.CIFAR10(root="data", train=True, download=False, transform=transform) 
+    test_set = torchvision.datasets.CIFAR10(root="data", train=False, download=False, transform=transform)
+
+    train_sampler = DistributedSampler(dataset=train_set)
+
+    train_loader = DataLoader(dataset=train_set, batch_size=512, sampler=train_sampler, num_workers=8)
+
+    # Test loader does not have to follow distributed sampling strategy
+    test_loader = DataLoader(dataset=test_set, batch_size=512, shuffle=False, num_workers=8)
+
     loss_fn = nn.MSELoss()
     optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
 
-    optimizer.zero_grad()
-    outputs = ddp_model(torch.randn(20, 10))
-    labels = torch.randn(20, 5).to(rank)
-    loss_fn(outputs, labels).backward()
-    optimizer.step()
+    device = torch.device("cuda:{}".format(rank))
+
+    for epoch in range(10):
+    	print("Local Rank: {}, Epoch: {}, Training ...".format(rank, epoch))
+        
+        # Save and evaluate model routinely
+        if epoch % 2 == 0:
+            if local_rank == 0:
+                accuracy = evaluate(model=ddp_model, device=device, test_loader=test_loader)
+                torch.save(ddp_model.state_dict(), model_filepath)
+                print("-" * 75)
+                print("Epoch: {}, Accuracy: {}".format(epoch, accuracy))
+                print("-" * 75)
+
+        ddp_model.train()
+
+        for data in train_loader:
+            inputs, labels = data[0].to(device), data[1].to(device)
+            optimizer.zero_grad()
+            outputs = ddp_model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+    # optimizer.zero_grad()
+    # outputs = ddp_model(torch.randn(20, 10))
+    # labels = torch.randn(20, 5).to(rank)
+    # loss_fn(outputs, labels).backward()
+    # optimizer.step()
 
     cleanup()
 
