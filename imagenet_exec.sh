10b1ee4424ef9e711e73ed9e0fe3f7d1bb49cf22
Ubuntu <ksreenivasa2@wisc.edu>
2022-04-29 16:59:55 +0000

    adding imagenet configs and exec script

---
+++ imagenet_exec.sh
@@ -0,0 +1,29 @@
+# Running trials in parallel
+# NOTE: make sure to delete/comment subfolder from the config file or else it may not work
+:<<BLOCK
+conf_file="configs/hypercube/wideresnet28/wideresnet28_weight_training.yml"
+log_root="wideresnet28_"
+log_end="_log"
+subfolder_root="wideresnet28_results_trial_"
+
+for trial in 2 3
+do
+    python main.py \
+    --config "$conf_file" \
+    --trial-num $trial \
+    --subfolder "$subfolder_root$trial" > "$log_root$trial$log_end" 2>&1 &
+
+    python main.py \
+    --config "$conf_file" \
+    --trial-num $trial \
+    --invert-sanity-check \
+    --subfolder "invert_$subfolder_root$trial" > "invert_$log_root$trial$log_end" 2>&1 &
+done
+
+BLOCK
+
+conf_file="configs/hypercube/resnet50/imagenet/resnet50_sparsity_5.yml"
+log_root="resnet50_sp5"
+log_end="_log"
+python main.py \
+    --config "$conf_file" > "$log_root$log_end" 2>&1 &
